1751339e7854d9a259b50cd5cda938a4f65154a2
zhaoyuanguang <zhaoyuanguang1573.@163.com>
2019-07-15 09:20:53 +0800

    springmvc:基础

---
+++ springmvc_basic/src/main/java/com/zyg/controller/HelloController.java
@@ -0,0 +1,31 @@
+package com.zyg.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+// 控制器类
+@Controller
+@RequestMapping(path="/user")
+public class HelloController {
+
+    /**
+     * hello
+     * @return
+     */
+    @RequestMapping(path="/hello")
+    public String sayHello(){
+        System.out.println("Hello StringMVC");
+        return "success";
+    }
+
+    /**
+     * RequestMapping注解
+     * @return
+     */
+    @RequestMapping(value="/testRequestMapping",params = {"username=heihei"},headers = {"Accept"})
+    public String testRequestMapping(){
+        System.out.println("测试RequestMapping注解...");
+        return "success";
+    }
+
+}
